f8fb70c96235de10d68b40c6cebee58efdf8a057
Josse Vandeputte <josse.vandeputte@student.howest.be>
2026-04-02 17:26:16 +0200

    feat: update project details

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,15 +7,27 @@ export default function Projects() {
     {
       id: 1,
       title: "Portfolio Website",
+      imageAlt: "Portfolio Website",
       description: "A responsive portfolio website.",
-      technologies: ["React - Next.js", "CSS Modules", "TypeScript"],
+      technologies: ["React", "CSS Modules", "TypeScript"],
       image: "/next.svg",
       demoUrl: "https://vdpj.be",
       codeUrl: "https://github.com/JosseVandeputte/Portfolio"
     },
-    {
+        {
       id: 2,
+      title: <>Vigilis Law Consult <br/> <span style={{ fontStyle: 'italic', fontSize: '0.75em', fontWeight: 'normal' }}>(updated version will be online at end of Q2 2026)</span></>,
+      imageAlt: "Vigilis Law Consult",
+      description: "A professional consulting platform for an expert in Belgian security law, offering legal advisory, law enforcement training, and public sector consulting.",
+      technologies: ["React", "CSS Modules", "TypeScript", "Deno"],
+      image: "/vlc.png",
+      demoUrl: "https://vigilislawconsult.be",
+      codeUrl: "https://github.com/JosseVandeputte/vigilislawconsult.be/tree/v2"    
+    },
+    {
+      id: 3,
       title: "Wordle",
+      imageAlt: "Wordle",
       description: "A clone of the popular Wordle game with a modern twist.",
       technologies: ["HTML", "CSS", "JavaScript"],
       image: "/wordle.png",
@@ -40,9 +52,9 @@ export default function Projects() {
               <div className={styles.projectImage}>
                 <Image
                   src={project.image}
-                  alt={project.title}
-                  width={60}
-                  height={60}
+                  alt={project.imageAlt}
+                  width={70}
+                  height={70}
                 />
               </div>
               

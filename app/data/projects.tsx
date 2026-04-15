43d0b835efb44f8226b14dc6668756bddc263d99
Josse Vandeputte <josse.vandeputte@student.howest.be>
2026-04-15 11:09:28 +0200

    fix: update technologies for projects and remove unused CSS Modules

--- app/data/projects.tsx
+++ app/data/projects.tsx
@@ -4,7 +4,7 @@ export const projects = [
     title: "Portfolio Website",
     imageAlt: "Portfolio Website",
     description: "A responsive portfolio website.",
-    technologies: ["React", "CSS Modules", "TypeScript"],
+    technologies: ["React", "TypeScript"],
     image: "/next.svg",
     demoUrl: "https://vdpj.be",
     codeUrl: "https://github.com/JosseVandeputte/Portfolio"
@@ -14,7 +14,7 @@ export const projects = [
     title: "Wordle",
     imageAlt: "Wordle",
     description: "A clone of the popular Wordle game with a modern twist.",
-    technologies: ["HTML", "CSS", "JavaScript"],
+    technologies: ["HTML", "JavaScript"],
     image: "/wordle.png",
     demoUrl: "https://wordle.vdpj.be",
     codeUrl: "https://github.com/JosseVandeputte/Wordle"
@@ -24,9 +24,29 @@ export const projects = [
     title: <>Vigilis Law Consult <br/> <span style={{ fontStyle: 'italic', fontSize: '0.75em', fontWeight: 'normal' }}>(updated version will be online at end of Q2 2026)</span></>,
     imageAlt: "Vigilis Law Consult",
     description: "A professional consulting platform for an expert in Belgian security law, offering legal advisory, law enforcement training, and public sector consulting.",
-    technologies: ["React", "CSS Modules", "TypeScript", "Deno"],
+    technologies: ["React", "TypeScript", "Deno"],
     image: "/vlc.png",
     demoUrl: "https://vigilislawconsult.be",
     codeUrl: "https://github.com/JosseVandeputte/vigilislawconsult.be/tree/v2"    
   },
+  {
+    id: 4,
+    title: "Netlify Clone",
+    imageAlt: "Netlify Clone",
+    description: "A clone of the popular Netlify platform with a modern twist.",
+    technologies: ["React", "TypeScript", "Spring Boot", "LocalStack (AWS)", "S3", "ECR", "ECS", "Docker"],
+    image: "/netlify.png",
+    demoUrl: "https://netlify.vdpj.be",
+    codeUrl: "https://github.com/JosseVandeputte/Netlify-Clone"
+  },
+  {
+    id: 5,
+    title: "RailSync",
+    imageAlt: "RailSync",
+    description: "An independent, real-time platform providing up-to-date travel information, delays, and departure times for the Belgian railways.",
+    technologies: ["React", "TypeScript", "iRail API"],
+    image: "/railsync.png",
+    demoUrl: "https://railsync.vdpj.be",
+    codeUrl: "",
+  }
 ];
